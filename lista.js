b2b66df0249fe03bc400abeab73e529248ddb6ed
claramdiaz <claramdiaz@hotmail.com>
2020-12-07 13:18:34 -0300

    lista

---
+++ lista.js
@@ -0,0 +1,13 @@
+let lista = [
+    {
+    nombre: "clara",
+    edad: 36,
+    localidad: "Vicente Lopez"
+    },
+    {
+        nombre: "Sabri",
+        edad: 36,
+        localidad: "San Isidro"
+    }
+]
+console.log(lista);
